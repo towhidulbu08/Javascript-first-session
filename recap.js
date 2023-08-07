334c70994380c264e7c2b1447246691ba98c6b03
towhidulbu08 <towhidulislam26121997@gmail.com>
2023-08-07 19:12:32 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,15 @@
+// var floating=34.344434;
+// var fixed=floating.toFixed(2)
+// console.log(fixed)
+// console.log(parseFloat(fixed))
+// console.log(typeof fixed)
+//post increment: x++;
+// pre increment:++x;
+var x=5;
+x++;
+console.log(x);
+console.log(++x);//age 1 barbe then print hobe..
+// console.log(x++);//age print hobe 5 then 1 barbe..
+// console.log(x++);
+// console.log(x++);
+
---
+++ ternary_operator.js
@@ -0,0 +1,19 @@
+let raining=true;
+// if(raining==true)
+// console.log("baire jabo na");
+// else
+//  console.log("jabo");
+//  raining ==false? console.log("baire jab na"): console.log("baire jabo");
+var age=25;
+var hasLicence=true;
+// if(age>=18){
+//     if(hasLicence==true)
+//     console.log("adult with licence");
+// else
+// console.log("adult with no licence");
+
+// }
+// else
+// console.log("you are not adult");
+
+age>=30 ? hasLicence==false? console.log("adult with licence") : console.log("adult with no licence") : console.log("you are not adult")
